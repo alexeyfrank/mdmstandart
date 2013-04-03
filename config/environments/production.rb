ddff03a1fa0b803fe625774e066a1b16952de147
Alexey Frank <alexeyfrank@gmail.com>
2013-04-03 20:31:08 +0400

    modify config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ MdmStandart::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
   config.assets.precompile += %w( ckeditor/* )
-  config.assets.precompile += %w( admin.js admin.css sign_in.js sign_in.css site.js site.css )
+  config.assets.precompile += %w( admin.js admin.css sign_in.js sign_in.css site.js site.css ie/ie.css.scss )
 
 
   # Disable delivery errors, bad email addresses will be ignored
